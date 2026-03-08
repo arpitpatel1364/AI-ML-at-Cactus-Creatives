68cbe17afb5e4f3546442cdfcb504ea023692245
Arpit <bhojaniarpit1432@gmail.com>
2026-03-08 20:26:46 +0530

    update matrix dot product

--- matrix task.py
+++ matrix task.py
@@ -11,7 +11,10 @@ print("\n--- Matrix 2 ---")
 print(matrix2)
 
 matrix_sum = matrix1 + matrix2
+matrix_product = matrix1 * matrix2
 
 print("\n--- Sum of Matrix 1 and Matrix 2 (All should be 1) ---")
 print(matrix_sum)
+print("\n--- Product of Matrix 1 and Matrix 2 (All should be 0) ---")
+print(matrix_product)
 
