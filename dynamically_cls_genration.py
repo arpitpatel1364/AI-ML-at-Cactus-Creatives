e41548542a8161a3f01da99d3f6fee838fd46328
Arpit <bhojaniarpit1432@gmail.com>
2026-03-10 15:18:32 +0530

    add some comments for better understand

--- dynamically_cls_genration.py
+++ dynamically_cls_genration.py
@@ -1,10 +1,9 @@
 def create_task_class(task_type):
-    """Generates a class customized for a specific task."""
+    """Generates a customised class for a specific task."""
     
     def perform(self):
         return f"Executing {self.task_name} logic for {task_type}..."
 
-    # Create the class dynamically
     class_name = f"{task_type.capitalize()}Task"
     
     return type(class_name, (object,), {
@@ -18,6 +17,5 @@ enc_task = create_task_class("encryption")
 for_name=create_task_class("for_name")
 
 
-# Instantiate and use
 my_pdf_job = PDFTask()
 print(my_pdf_job.execute()) 
